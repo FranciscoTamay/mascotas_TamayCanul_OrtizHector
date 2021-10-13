a7b4407de16179503284a3ba9a160e0d87d90950
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2021-10-13 18:14:56 -0500

    Initial commit

---
+++ app/Mascota.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mascota extends Model
+{
+    protected $table= 'mascotas';
+    protected $primaryKey='id_mascota';
+
+    //especificamos las relaciones
+    public $with=['especie'];
+    //define si la llave primaria es o no numero autoincrementable
+    public $incrementing= true; 
+
+    //activar o desactivar etiquetas de tiempo 
+    public $timestamps=true;
+
+    public $fillable=[
+        'id_mascota',
+        'nombre',
+        'edad',
+        'peso',
+        'genero',
+        'id_especie',
+        'id_propietario'
+
+    ];
+
+    public function especie()
+    {
+        return $this->belongsTo(Especie::class, 'id_especie','id_especie');
+    }
+
+
+}
+
