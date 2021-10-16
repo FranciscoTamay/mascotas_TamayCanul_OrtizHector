962014e3bdea634374296b60bc4acfc0ab3051bc
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2021-10-15 20:42:59 -0500

    Modelo raza
    se creo el modelo raza

---
+++ app/Raza.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Raza; extends Model
+{
+    //
+    protected $tabla='raza';
+    protected $primaryKey='id_raza';
+
+    public $incrementing=true;
+
+    public $timestamps=false;
+
+    protected $fillable=[
+        'id_raza',
+        'raza'
+
+    ];
+}
